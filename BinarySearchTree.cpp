82be0f3bbddc9191c13f9fee1bbf9bf912a17301
Pham Doan Tien 19127576 <63442323+NEITGNART@users.noreply.github.com>
2020-09-26 14:52:25 +0700

    Update BinarySearchTree.cpp

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -31,7 +31,7 @@ public:
 			if (key < q->key) {
 				if (q->left == nullptr) {
 					q->left = new Node(key);;
-					cout << "Insert " << key << " successfully!\n";
+			//		cout << "Insert " << key << " successfully!\n";
 					return;
 				}
 				q = q->left;
@@ -40,7 +40,7 @@ public:
 			if (key > q->key) {
 				if (q->right == nullptr) {
 					q->right = new Node(key);
-					cout << "Insert " << key << " successfully!\n";
+				//	cout << "Insert " << key << " successfully!\n";
 					return;
 				}
 				q = q->right;
@@ -48,15 +48,68 @@ public:
 			}
 		}
 	}
-	void Traversal(Node *root, int depth) {
-		if (root != nullptr) {
-			for (int i = 0; i < depth; ++i) cout << "  ";
-			cout << root->key << " " << endl;
-			Traversal(root->left, depth+1);
-			Traversal(root->right, depth+1);
+	struct Trunk
+	{
+		Trunk* prev;
+		string str;
+
+		Trunk(Trunk* prev, string str)
+		{
+			this->prev = prev;
+			this->str = str;
 		}
+	};
+
+	// Helper function to print branches of the binary tree
+	void showTrunks(Trunk* p)
+	{
+		if (p == nullptr)
+			return;
+
+		showTrunks(p->prev);
+
+		cout << p->str;
+	}
+
+	// Recursive function to print binary tree
+	// It uses inorder traversal
+	void printTree(Node* root, Trunk* prev, bool isLeft)
+	{
+		if (root == nullptr)
+			return;
+
+		string prev_str = "	";
+		Trunk* trunk = new Trunk(prev, prev_str);
+
+		printTree(root->left, trunk, true);
+
+		if (!prev)
+			trunk->str = "---";
+		else if (isLeft)
+		{
+			trunk->str = ".---";
+			prev_str = "	|";
+		}
+		else
+		{
+			trunk->str = "`---";
+			prev->str = prev_str;
+		}
+
+		showTrunks(trunk);
+		cout << root->key << endl;
+
+		if (prev)
+			prev->str = prev_str;
+		trunk->str = "	|";
+
+		printTree(root->right, trunk, false);
 	}
 };
+
+
+
+
 
 int main(void) {
 
@@ -73,7 +126,7 @@ int main(void) {
 	tree.Insert(11);
 	
 
-	tree.Traversal(root, 1);
+	tree.printTree(root, nullptr, false);
 	
 	
 	return 0;
