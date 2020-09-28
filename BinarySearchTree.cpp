bccb868c15479fd1711a488258f968249d760c4c
Pham Doan Tien 19127576 <63442323+NEITGNART@users.noreply.github.com>
2020-09-28 21:24:45 +0700

    Update BinarySearchTree.cpp

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -45,6 +45,7 @@ public:
 				}
 				q = q->right;
 				continue;
+				
 			}
 		}
 	}
@@ -70,7 +71,15 @@ public:
 
 		cout << p->str;
 	}
+	void Traversal(Node *root) {
 
+		if (root == nullptr) return;
+			Traversal(root->left);
+			cout << root->key << " ";
+			Traversal(root->right);
+			
+		
+	}
 	// Recursive function to print binary tree
 	// It uses inorder traversal
 	void printTree(Node* root, Trunk* prev, bool isLeft)
@@ -105,6 +114,44 @@ public:
 
 		printTree(root->right, trunk, false);
 	}
+
+	Node *FindMin(Node *root) {
+
+		while (root && root->left) root = root->left;
+		return root;
+	}
+	
+	Node *Delete(Node *root, int x) {
+
+		if (root == nullptr) return root;
+		else if (root->key > x) root->left = Delete(root->left, x);
+		else if (root->key < x) root->right = Delete(root->right, x);
+		else {
+			if (root->left == nullptr && root->right == nullptr) {
+				delete root;
+				root = nullptr;
+			}
+			else if (root->left == nullptr) {
+				Node* temp = root;
+				root = root->right;
+				delete temp;
+
+			}
+			else if (root->right == nullptr) {
+				Node* temp = root;
+				root = root->left;
+				delete temp;
+			}
+			else {
+				Node* temp = FindMin(root->right);
+				root->key = temp->key;
+				root->right = Delete(root->right, temp->key);
+			}
+			
+		}
+		return root;
+	}
+
 };
 
 
@@ -124,8 +171,9 @@ int main(void) {
 	tree.Insert(9);
 	tree.Insert(8);
 	tree.Insert(11);
-	
 
+	//root = tree.Delete(root, 7);
+	
 	tree.printTree(root, nullptr, false);
 	
 	
